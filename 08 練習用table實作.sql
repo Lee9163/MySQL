7c839c0a4c7ac33877bcd5106e4a8392ccb2a3bf
Lee9163 <dennyjason67@gmail.com>
2025-04-27 19:53:25 +0800

    Update 08 練習用table實作.sql

--- 08 練習用table實作.sql
+++ 08 練習用table實作.sql
@@ -1,7 +1,11 @@
 #練習
 #1.取得所有員工資料
-select
+select * from `employee`;
 #2.取得所有客戶資料
+select * from `client`;
 #3.按薪水低到高取得員工資料
+select * from `employee` order by `salary`;
 #4.取得薪水前三高的員工
+select `name` from `employee` order by `salary` desc limit 3;
 #5.取得所有員工的名字
+select `name` from `employee` ;
